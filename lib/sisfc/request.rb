bd484cac7ae574e5fdd6247f641ad85b2840aa50
Mauro Tortonesi <mauro.tortonesi@unife.it>
2018-05-28 16:33:11 +0200

    Minor changes
    - switched to function arguments;
    - updated/improved comments.

--- lib/sisfc/request.rb
+++ lib/sisfc/request.rb
@@ -13,19 +13,18 @@ module SISFC
                 :generation_time,
                 :next_step,
                 # :status,
-                :workflow_type_id 
+                :workflow_type_id
 
-    # the data_center_id attribute is initially set at request creation time
-    # (typically from the request generator) and then updated if requests move
-    # from a Cloud data center to another
+    # the data_center_id attribute is updated as requests move from a Cloud
+    # data center to another
     attr_accessor :data_center_id
 
-    def initialize(rid,
-                   generation_time,
-                   initial_data_center_id,
-                   arrival_time,
-                   workflow_type_id,
-                   customer_id)
+    def initialize(rid:,
+                   generation_time:,
+                   initial_data_center_id:,
+                   arrival_time:,
+                   workflow_type_id:,
+                   customer_id:)
       @rid              = rid
       @generation_time  = generation_time
       @data_center_id   = initial_data_center_id
@@ -33,7 +32,7 @@ module SISFC
       @workflow_type_id = workflow_type_id
       @customer_id      = customer_id
 
-      # steps count from zero
+      # steps start counting from zero
       @next_step = 0
 
       # calculate communication latency
