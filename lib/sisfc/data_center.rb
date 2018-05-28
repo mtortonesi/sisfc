16d35af2303c23f600f89a3bbab07459a4cfbf5b
Mauro Tortonesi <mauro.tortonesi@unife.it>
2018-05-28 15:53:42 +0200

    Minor overhaul
    - switched constructor to function arguments;
    - added has_vms_of_type? method;
    - added tracking of number of allocated VMs according to their type;
    - added optional availability check before VM allocation according to a
      user-specified procedure;
    - removed useless and leaky get_number_of_vms method;
    - (temporarily?) removed available method.

--- lib/sisfc/data_center.rb
+++ lib/sisfc/data_center.rb
@@ -1,33 +1,43 @@
+require 'forwardable'
+
 module SISFC
 
   class DataCenter
+    extend Forwardable
+
+    def_delegator :@vms, :has_key?, :has_vms_of_type?
 
     attr_reader :dcid, :location_id
 
-    def initialize(id, opts)
-      @dcid        = id
-      @available   = opts[:maximum_vm_capacity]
-      @capacity    = @available.dup.freeze
-      @vms         = {}
-      @location_id = opts[:location_id]
+    def initialize(id:, location_id:, **opts)
+      @dcid          = id
+      @location_id   = location_id
+      @vms           = {}
+      @vm_type_count = {}
+      @availability_check_proc = opts[:maximum_vm_capacity]
     end
 
+    # returns false in case no more VMs can be allocated
     def add_vm(vm, component_name)
       @vms[component_name] ||= []
+      @vm_type_count[vm.size] ||= 0
 
       raise 'Error! VM is already present!' if @vms[component_name].include? vm
 
-      # return an error code if we don't have any more VMs available
-      return false unless @available[vm.size] > 0
+      # defer availablility check to user specified procedure
+      if @availability_check_proc
+        return false unless @availability_check_proc.call(@vm_type_count)
+      end
 
       # allocate VM
-      @available[vm.size] -= 1
       @vms[component_name] << vm
+      @vm_type_count[vm.size] += 1
     end
 
     def remove_vm(vm, component_name)
       if @vms.has_key? component_name and @vms[component_name].include? vm
-        @available[vm.size] += 1
+        raise 'Error! Inconsistent number of VMs!' unless @vm_type_count[vm.size] >= 1
+        @vm_type_count[vm.size] += 1
         @vms.delete(vm)
       end
     end
@@ -37,14 +47,6 @@ module SISFC
       if @vms.has_key? component_name
         @vms[component_name].sample
       end
-    end
-
-    def get_number_of_vms(component_name)
-      @vms.has_key? component_name ? @vms[component_name].size : 0
-    end
-
-    def available(vm_size)
-      @available[vm_size]
     end
 
     def to_s
---
+++ spec/sisfc/data_center_spec.rb
@@ -0,0 +1,12 @@
+require 'minitest_helper'
+
+require_relative './reference_configuration'
+
+
+describe SISFC::DataCenter do
+  it 'should create a valid data center' do
+    args = { maximum_vm_capacity: lambda {|vms| true } }
+    dc = SISFC::DataCenter.new(id: :dc_name, location_id: 1, **args)
+  end
+end
+
